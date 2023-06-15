6d5691098c1bb96af2f0afaf6d164acfb352acdd
Miguel Mañas <miguel.manas91@gmail.com>
2023-06-15 12:52:37 +0200

    content now only prints after finishing fetch

--- js/main.js
+++ js/main.js
@@ -16,22 +16,23 @@ document.querySelector("#searchInput").addEventListener("keydown", (e) => {
 
 document.querySelector("#order").addEventListener("click", () => {});
 
-// API url for searches. Only search ones that have images. This gives a list of IDs.
+// API url for searches. Only search ones that have images and are highlight. This gives a list of IDs.
 const searchUrl =
-    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=";
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&q=";
 
 //  function to get array of ID results from search.
 async function searchIds(query) {
     const response = await fetch(searchUrl + query);
     const entries = await response.json();
-    loadObjectInfo(entries);
+    await loadObjectInfo(entries);
+    printContent();
 }
 
 // API url for getting data from each ID.
 const objectInfoUrl =
     "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
 
-const results = [];
+let results = [];
 
 // function to load data from API for each Id entry. If the entry is in public domain, it pushes it to results.
 async function loadObjectInfo(entries) {
@@ -41,21 +42,23 @@ async function loadObjectInfo(entries) {
         const data = await response.json();
         if (data.isPublicDomain) {
             results.push(data);
-            printContent(data); //!!!
+            console.log(results);
         }
     }
 }
 
-function printContent(data) {
+function printContent() {
     const fragment = document.createDocumentFragment();
     const template = document.querySelector("#artwork").content;
-    const clone = template.cloneNode(true);
-    clone.querySelector(".picture").src = data.primaryImageSmall;
-    clone.querySelector(".picture").alt = data.title;
-    clone.querySelector(".title").innerText = data.title;
-    clone.querySelector(".author").innerText = data.artistDisplayName;
-    clone.querySelector(".year").innerText = data.objectDate;
-    fragment.appendChild(clone);
+    for (const result of results) {
+        const clone = template.cloneNode(true);
+        clone.querySelector(".picture").src = result.primaryImageSmall;
+        clone.querySelector(".picture").alt = result.title;
+        clone.querySelector(".title").innerText = result.title;
+        clone.querySelector(".author").innerText = result.artistDisplayName;
+        clone.querySelector(".year").innerText = result.objectDate;
+        fragment.appendChild(clone);
+    }
     document.querySelector("#container").appendChild(fragment);
 }
 
@@ -65,6 +68,6 @@ searchIds("van gogh"); // initial call to show images at first load.
 
 function clearContent() {
     // Previous load function is not interrupted!!!.
-    // const results = [];
-    // document.querySelector("#container").replaceChildren();
+    results = [];
+    document.querySelector("#container").replaceChildren();
 }
