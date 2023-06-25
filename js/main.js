9004b3b507f2abac4e514c4e5243eb0e4bdea354
Miguel Mañas <miguel.manas91@gmail.com>
2023-06-26 00:51:51 +0200

    updated header

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 // Search query from input clicking button.
-document.querySelector("#searchButton").addEventListener("click", () => {
-    clearContent();
-    const query = document.querySelector("#searchInput").value;
+document.querySelector("#search-button").addEventListener("click", () => {
+    // clearContent();
+    const query = document.querySelector("#search-input").value;
     searchIds(query);
 });
 
 // Search query from input by pressing Enter.
-document.querySelector("#searchInput").addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        clearContent();
-        const query = document.querySelector("#searchInput").value;
+document.querySelector("#search-input").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        // clearContent();
+        const query = document.querySelector("#search-input").value;
         searchIds(query);
     }
 });
 
-document.querySelector("#order").addEventListener("click", () => {});
+// document.querySelector("#sort").addEventListener("click", () => {});
 
-// API url for searches. Only search ones that have images and are highlight. This gives a list of IDs.
-const searchUrl =
-    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&q=";
+let results = [];
 
 //  function to get array of ID results from search.
 async function searchIds(query) {
+    // API url for searches. Only search ones that have images and are highlight. This gives a list of IDs.
+    const searchUrl =
+        "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&q=";
     const response = await fetch(searchUrl + query);
     const entries = await response.json();
     await loadObjectInfo(entries);
@@ -29,13 +30,11 @@ async function searchIds(query) {
 }
 
 // API url for getting data from each ID.
-const objectInfoUrl =
-    "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
-
-let results = [];
 
 // function to load data from API for each Id entry. If the entry is in public domain, it pushes it to results.
 async function loadObjectInfo(entries) {
+    const objectInfoUrl =
+        "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
     for (const entry of entries.objectIDs) {
         const info = objectInfoUrl + entry;
         const response = await fetch(info);
@@ -49,7 +48,7 @@ async function loadObjectInfo(entries) {
 
 function printContent() {
     const fragment = document.createDocumentFragment();
-    const template = document.querySelector("#artwork").content;
+    const template = document.querySelector("#artworks").content;
     for (const result of results) {
         const clone = template.cloneNode(true);
         clone.querySelector(".picture").src = result.primaryImageSmall;
@@ -59,15 +58,13 @@ function printContent() {
         clone.querySelector(".year").innerText = result.objectDate;
         fragment.appendChild(clone);
     }
-    document.querySelector("#container").appendChild(fragment);
+    document.querySelector(".container-artworks").appendChild(fragment);
 }
-
-searchIds("van gogh"); // initial call to show images at first load.
 
 // Clear previous loaded content when a new search is made
 
 function clearContent() {
     // Previous load function is not interrupted!!!.
     results = [];
-    document.querySelector("#container").replaceChildren();
+    document.querySelector(".container-artworks").replaceChildren();
 }
