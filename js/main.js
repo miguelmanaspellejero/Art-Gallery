3c8c774a57e239dd1c2e15451727c1c3872d1936
Miguel Mañas <miguel.manas91@gmail.com>
2023-06-29 18:39:13 +0200

    disable input until current search is finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const results = [];
-let newSearch = false;
+let searchRunning = false; // This avoids new searches from being started until current is finished.
 
 // Click and enter search events
 document.querySelector("#search-button").addEventListener("click", startSearch);
@@ -12,8 +12,10 @@ document.querySelector("#search-input").addEventListener("keydown", (event) => {
 // Start search query from input
 function startSearch() {
     const query = document.querySelector("#search-input").value;
-    clearContent();
-    searchIds(query);
+    if (searchRunning === false && query.length > 0) {
+        clearContent();
+        searchIds(query);
+    }
 }
 
 function showStatus(query, status) {
@@ -35,6 +37,7 @@ function showStatus(query, status) {
 
 //  function to get array of ID results from search.
 async function searchIds(query) {
+    controlSearches(true);
     // API url for searches. Only search ones that have images and are highlight. This gives a list of IDs.
     const searchUrl =
         "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&q=";
@@ -43,10 +46,22 @@ async function searchIds(query) {
     showStatus(query, "loading");
     if (entries.objectIDs === null) {
         showStatus(query, "notFound");
+        controlSearches(false);
+        return;
     }
     await loadObjectInfo(entries);
     await printContent();
     showStatus(query, "finished");
+    controlSearches(false);
+}
+
+function controlSearches(value) {
+    searchRunning = value;
+    document
+        .querySelector(".container-search")
+        .classList.toggle("disabled", searchRunning);
+    document.querySelector("#search-button").disabled = value;
+    document.querySelector("#search-input").disabled = value;
 }
 
 // function to load data from API for each Id entry. If the entry is in public domain, it pushes it to results.
@@ -67,9 +82,6 @@ async function printContent() {
     const fragment = document.createDocumentFragment();
     const template = document.querySelector("#artworks").content;
     for (const result of results) {
-        if (newSearch) {
-            break;
-        }
         const clone = template.cloneNode(true);
         clone.querySelector(".picture").src = result.primaryImageSmall;
         clone.querySelector(".picture").alt = result.title;
