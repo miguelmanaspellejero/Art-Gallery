e777313b5b8bc9dd1a45274cb217e5470fdf7883
Miguel Mañas <miguel.manas91@gmail.com>
2023-07-21 21:25:47 +0200

    favorites page with carrousel

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,51 @@
 import { printHeaderAndFooter } from "./modules/templates.js";
+import { loadFavorites, saveFavorites } from "./modules/storage.js";
 
 await printHeaderAndFooter();
+document.querySelector(".link-favorites").classList.add("active-link");
+
+if (loadFavorites().length > 0) {
+    printFavorites();
+    printFavStatus();
+} else {
+    printNoFavoritesMessage();
+}
+
+function printFavorites() {
+    const favorites = loadFavorites();
+    const favFragment = document.createDocumentFragment();
+    const template = document.querySelector("#favorite-artworks").content;
+    const slideFragment = document.createDocumentFragment();
+    for (const favorite of favorites) {
+        const clone = template.cloneNode(true);
+        const favImage = clone.querySelector(".favorite-image");
+        favImage.id = favorite.id;
+        favImage.src = favorite.imageLarge;
+        favImage.alt = favorite.title;
+        favImage.addEventListener("click", () => {
+            window.location.href = `detail.html?id=${favorite.id}`;
+        });
+        favFragment.appendChild(clone);
+        const slideLink = document.createElement("a");
+        slideLink.href = "#" + favorite.id;
+        slideFragment.append(slideLink);
+    }
+
+    document.querySelector(".slider-nav").appendChild(slideFragment);
+    document.querySelector(".container-favorites").appendChild(favFragment);
+
+    for (const favorite of favorites) {
+    }
+}
+
+function printFavStatus() {
+    document.querySelector(".status-message").textContent = `Showing ${
+        loadFavorites().length
+    } favorites in your collection`;
+}
+
+function printNoFavoritesMessage() {
+    document
+        .querySelector(".no-favorites-message")
+        .classList.add("display-flex");
+}
